6b5f0be5d2a9160663625bd17133091b74801df1
Quy Nguyen Duy <duyquy140903@gmail.com>
2024-09-24 12:37:18 +0700

    Update jumping, crouching animation

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    public float runSpeed = 100f;
+    bool crouch = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+            animator.SetBool("IsCrouching", true);
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+            animator.SetBool("IsCrouching", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+}
